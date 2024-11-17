d1085fa0cb59285b69b81d01fa647438299760ce
Rodas-T <rodastlb@gmail.com>
2024-11-17 17:22:24 +0300

    app commited

--- src/components/YouTube/YouTube.js
+++ src/components/YouTube/YouTube.js
@@ -4,14 +4,16 @@ function YouTube() {
   const [youTube, setYouTube] = useState([]);
 
   useEffect(() => {
+    const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY; // Access the key
     fetch(
-      "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDbLBFMBxuDfejGFK1GWfRlTWo8033aJKU&channelId=UCE_M8A5yxnLfW0KghEeajjw&part=snippet,id&order=date&maxResults=6"
+      `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=UCE_M8A5yxnLfW0KghEeajjw&part=snippet,id&order=date&maxResults=6`
     )
       .then((response) => response.json())
       .then((data) => {
         const itemData = data.items;
         setYouTube(itemData);
-      });
+      })
+      .catch((error) => console.error("Error fetching YouTube data:", error));
   }, []);
 
   return (
@@ -19,7 +21,9 @@ function YouTube() {
       <div className="container">
         <div className="row h-100 align-items-center justify-content-center text-center">
           <div className="col-12">
-            <div className="title-wrapper bold video-title-wrapper">Latest Videos</div>
+            <div className="title-wrapper bold video-title-wrapper">
+              Latest Videos
+            </div>
           </div>
           {youTube.map((singleVideo) => {
             const vidId = singleVideo.id.videoId;
@@ -29,16 +33,25 @@ function YouTube() {
                 <div className="singleVideoWrapper">
                   <div className="videoThumbnail">
                     <a href={vidLink} target="_blank" rel="noopener noreferrer">
-                      <img src={singleVideo.snippet.thumbnails.high.url} alt="video thumbnail" />
+                      <img
+                        src={singleVideo.snippet.thumbnails.high.url}
+                        alt="video thumbnail"
+                      />
                     </a>
                   </div>
                   <div className="videoInfoWrapper">
                     <div className="videoTitle">
-                      <a href={vidLink} target="_blank" rel="noopener noreferrer">
+                      <a
+                        href={vidLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
                         {singleVideo.snippet.title}
                       </a>
                     </div>
-                    <div className="videoDesc">{singleVideo.snippet.description}</div>
+                    <div className="videoDesc">
+                      {singleVideo.snippet.description}
+                    </div>
                   </div>
                 </div>
               </div>
